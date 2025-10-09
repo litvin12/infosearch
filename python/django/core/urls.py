a8eb94520057ea1c612569ee2b9d056cc3886dd2
a.voikov <a.voikov@g.nsu.ru>
2025-10-10 01:18:43 +0700

    only view

--- python/django/core/urls.py
+++ python/django/core/urls.py
@@ -1,7 +1,9 @@
 from django.urls import path
+from django.views.generic import RedirectView
 from . import views
 
 urlpatterns = [
+    path('', RedirectView.as_view(url='/universities/')),
     path('universities/', views.UniversityListView.as_view(), name='university_list'),
     path('universities/<int:pk>/', views.UniversityDetailView.as_view(), name='university_detail'),
     path('students/', views.StudentListView.as_view(), name='student_list'),
