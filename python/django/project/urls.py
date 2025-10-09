7667ae862b3b37f861752b479b5734b3d8444086
a.voikov <a.voikov@g.nsu.ru>
2025-10-10 00:26:06 +0700

    started

---
+++ python/django/project/urls.py
@@ -0,0 +1,27 @@
+"""
+URL configuration for project project.
+
+The `urlpatterns` list routes URLs to views. For more information please see:
+    https://docs.djangoproject.com/en/5.2/topics/http/urls/
+Examples:
+Function views
+    1. Add an import:  from my_app import views
+    2. Add a URL to urlpatterns:  path('', views.home, name='home')
+Class-based views
+    1. Add an import:  from other_app.views import Home
+    2. Add a URL to urlpatterns:  path('', Home.as_view(), name='home')
+Including another URLconf
+    1. Import the include() function: from django.urls import include, path
+    2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
+"""
+from django.contrib import admin
+from django.urls import path
+from . import views
+
+urlpatterns = [
+    path('admin/', admin.site.urls),
+    path('universities/', views.UniversityListView.as_view(), name='university_list'),
+    path('universities/<int:pk>/', views.UniversityDetailView.as_view(), name='university_detail'),
+    path('students/', views.StudentListView.as_view(), name='student_list'),
+    path('students/<int:pk>/', views.StudentDetailView.as_view(), name='student_detail'),
+]
