d2b5288f5c0164635435e22e98685392de36f9ce
a.voikov <150669776+litvin12@users.noreply.github.com>
2025-10-10 16:26:02 +0000

    123

--- python/django/core/urls.py
+++ python/django/core/urls.py
@@ -8,4 +8,10 @@ urlpatterns = [
     path('universities/<int:pk>/', views.UniversityDetailView.as_view(), name='university_detail'),
     path('students/', views.StudentListView.as_view(), name='student_list'),
     path('students/<int:pk>/', views.StudentDetailView.as_view(), name='student_detail'),
+    path('universities/create/', views.UniversityCreateView.as_view(), name='university_create'),
+    path('universities/<int:pk>/update/', views.UniversityUpdateView.as_view(), name='university_update'),
+    path('universities/<int:pk>/delete/', views.UniversityDeleteView.as_view(), name='university_delete'),
+    path('students/create/', views.StudentCreateView.as_view(), name='student_create'),
+    path('students/<int:pk>/update/', views.StudentUpdateView.as_view(), name='student_update'),
+    path('students/<int:pk>/delete/', views.StudentDeleteView.as_view(), name='student_delete'),
 ]
--- python/django/core/views.py
+++ python/django/core/views.py
@@ -1,5 +1,7 @@
 from django.views.generic import ListView, DetailView
+from django.views.generic.edit import CreateView, UpdateView, DeleteView
 from .models import University, Student
+from django.urls import reverse_lazy
 
 class UniversityListView(ListView):
     model = University
@@ -11,6 +13,23 @@ class UniversityDetailView(DetailView):
     template_name = 'university_detail.html'
     context_object_name = 'university'
 
+class UniversityCreateView(CreateView):
+    model = University
+    fields = ['full_name', 'short_name', 'established_date']
+    template_name = 'university_form.html'
+    success_url = reverse_lazy('university_list')
+
+class UniversityUpdateView(UpdateView):
+    model = University
+    fields = ['full_name', 'short_name', 'established_date']
+    template_name = 'university_form.html'
+    success_url = reverse_lazy('university_list')
+
+class UniversityDeleteView(DeleteView):
+    model = University
+    template_name = 'university_confirm_delete.html'
+    success_url = reverse_lazy('university_list')
+
 class StudentListView(ListView):
     model = Student
     template_name = 'student_list.html'
@@ -20,3 +39,20 @@ class StudentDetailView(DetailView):
     model = Student
     template_name = 'student_detail.html'
     context_object_name = 'student'
+
+class StudentCreateView(CreateView):
+    model = Student
+    fields = ['full_name', 'birth_date', 'university', 'enrollment_year']
+    template_name = 'student_form.html'
+    success_url = reverse_lazy('student_list')
+
+class StudentUpdateView(UpdateView):
+    model = Student
+    fields = ['full_name', 'birth_date', 'university', 'enrollment_year']
+    template_name = 'student_form.html'
+    success_url = reverse_lazy('student_list')
+
+class StudentDeleteView(DeleteView):
+    model = Student
+    template_name = 'student_confirm_delete.html'
+    success_url = reverse_lazy('student_list')
